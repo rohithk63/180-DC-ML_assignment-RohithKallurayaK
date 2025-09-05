10255d4960c65a75e8795a3f4d010dfd9c506982
rohithkalluraya <rohithkalluraya@gmail.com>
2025-09-05 14:57:34 +0530

    Delete challenge_3 directory

--- challenge_3/backendAPI/judge.py
+++
@@ -1,7 +0,0 @@
-def judge_decision(request):
-    if request.winner:
-        return {"verdict": f"Judge declares {request.winner} the winner!"}
-    elif request.new_evidence:
-        return {"verdict": f"New evidence introduced: {request.new_evidence}"}
-    else:
-        return {"verdict": "No decision made yet."}
